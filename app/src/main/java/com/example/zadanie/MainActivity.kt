8e5e22137fe4de7e264c7de762211a15eb2ec7fe
erikgreasy <erik.masny@gmail.com>
2022-12-18 13:43:36 +0100

    feat: show bottom navigation only on specific views

--- app/src/main/java/com/example/zadanie/MainActivity.kt
+++ app/src/main/java/com/example/zadanie/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.zadanie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
+val bottomNavFragments = listOf<Int>(
+    R.id.bars_fragment,
+    R.id.friendsFragment
+)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,17 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(bottomNavFragments.contains(destination.id)) {
+                bottomNavigationView.visibility = View.VISIBLE
+            } else {
+                bottomNavigationView.visibility = View.GONE
+            }
+        }
+
         findViewById<BottomNavigationView>(R.id.nav_view)
             .setupWithNavController(navController)
     }
